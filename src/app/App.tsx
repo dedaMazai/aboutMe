1497ebca523ad92e62f20fe2b939d37e4ae3cf0e
Andrey Chipizubov <andryukha.chipizybov@mail.ru>
2022-09-26 23:41:40 +0300

    fix bug

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,18 @@
-import { Link, Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
-import {AboutPage} from "pages/AboutPage/";
-import {MainPage} from "pages/MainPage/";
+import { Link } from "react-router-dom";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
-import { className } from "shared/lib/classNames/classNames";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Nuvbar";
 
 import './styles/index.scss'
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme()
     return (
-        <div className={className('app', {}, [theme])}>
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
+            <AppRouter/>
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
         </div>
     )
 }
