be9f09ee03afbc78cbc34990afc60b0deac3c846
Andrey Chipizubov <andryukha.chipizybov@mail.ru>
2022-10-01 18:35:28 +0300

    fix bug

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
@@ -6,15 +7,19 @@ interface NavbarProps {
   className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink to="/" className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>
-                Главная
-            </AppLink>
-            <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-                О сайте
-            </AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink to="/" className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>
+                    {t('main')}
+                </AppLink>
+                <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
+                    {t('aboutSite')}
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
