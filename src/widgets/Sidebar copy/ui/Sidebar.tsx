b9ba7872131e44d9f5f201f8c8b2ff8df7468525
Andrey Chipizubov <andryukha.chipizybov@mail.ru>
2022-10-05 19:47:28 +0300

    fix

---
+++ src/widgets/Sidebar copy/ui/Sidebar.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { LangSwitcher } from 'shared/ui/LangSwitcher';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+import cls from './Sidebar.module.scss';
+
+interface SidebarProps {
+  className?: string
+}
+
+export const Sidebar = ({ className }: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
+    const onToggle = () => {
+        setCollapsed((prev) => !prev);
+    };
+
+    return (
+        <div
+            data-testid="sidebar"
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+        >
+            <button
+                data-testid="sidebar-toggle"
+                onClick={onToggle}
+                type="button"
+            >
+                {t('toggle')}
+            </button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+                <LangSwitcher className={cls.lang} />
+            </div>
+        </div>
+    );
+};
