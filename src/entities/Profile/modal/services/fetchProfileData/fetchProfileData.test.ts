5dd740b55871cf932bc6a52634ed2f30c050e935
Andrey Chipizubov <andryukha.chipizybov@mail.ru>
2022-11-20 19:16:49 +0300

    fix

--- src/entities/Profile/modal/services/fetchProfileData/fetchProfileData.test.ts
+++ src/entities/Profile/modal/services/fetchProfileData/fetchProfileData.test.ts
@@ -1,35 +1,35 @@
-// import { Country } from 'entities/Country';
-// import { Currency } from 'entities/Currency';
-// import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
-// import { fetchProfileData } from './fetchProfileData';
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+import { fetchProfileData } from './fetchProfileData';
 
-// const data = {
-//     username: 'admin',
-//     age: 22,
-//     country: Country.Armenia,
-//     lastname: 'ulbi tv',
-//     first: 'abs',
-//     city: 'abs',
-//     currency: Currency.EUR,
-// };
+const data = {
+    username: 'admin',
+    age: 22,
+    country: Country.Armenia,
+    lastname: 'ulbi tv',
+    first: 'abs',
+    city: 'abs',
+    currency: Currency.EUR,
+};
 
-// describe('fetchProfileData.test', () => {
-//     test('success', async () => {
-//         const thunk = new TestAsyncThunk(fetchProfileData);
-//         thunk.api.get.mockReturnValue(Promise.resolve({ data }));
+describe('fetchProfileData.test', () => {
+    test('success', async () => {
+        const thunk = new TestAsyncThunk(fetchProfileData);
+        thunk.api.get.mockReturnValue(Promise.resolve({ data }));
 
-//         const result = await thunk.callThunk('1');
+        const result = await thunk.callThunk('1');
 
-//         expect(thunk.api.get).toHaveBeenCalled();
-//         expect(result.meta.requestStatus).toBe('fulfilled');
-//         expect(result.payload).toEqual(data);
-//     });
+        expect(thunk.api.get).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(result.payload).toEqual(data);
+    });
 
-//     test('error', async () => {
-//         const thunk = new TestAsyncThunk(fetchProfileData);
-//         thunk.api.get.mockReturnValue(Promise.resolve({ status: 403 }));
-//         const result = await thunk.callThunk('1');
+    test('error', async () => {
+        const thunk = new TestAsyncThunk(fetchProfileData);
+        thunk.api.get.mockReturnValue(Promise.resolve({ status: 403 }));
+        const result = await thunk.callThunk('1');
 
-//         expect(result.meta.requestStatus).toBe('rejected');
-//     });
-// });
+        expect(result.meta.requestStatus).toBe('rejected');
+    });
+});
--- src/pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.test.ts
+++ src/pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.test.ts
@@ -1,45 +1,45 @@
-// import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
-// import { ArticlePageActions } from '../../slices/ArticlePageSlice';
-// import { fetchArticleList } from '../fetchArticleList/fetchArticleList';
-// import { fetchNextArticlesPage } from './fetchNextArticlesPage';
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+import { ArticlePageActions } from '../../slices/ArticlePageSlice';
+import { fetchArticleList } from '../fetchArticleList/fetchArticleList';
+import { fetchNextArticlesPage } from './fetchNextArticlesPage';
 
-// jest.mock('../fetchArticleList/fetchArticleList');
-// jest.mock('../../slices/ArticlePageSlice');
+jest.mock('../fetchArticleList/fetchArticleList');
+jest.mock('../../slices/ArticlePageSlice');
 
-// describe('fetchNextArticlesPage.test', () => {
-//     test('success', async () => {
-//         const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
-//             articlePage: {
-//                 page: 2,
-//                 ids: [],
-//                 entities: {},
-//                 limit: 5,
-//                 isLoading: false,
-//                 hasMore: true,
-//             },
-//         });
+describe('fetchNextArticlesPage.test', () => {
+    test('success', async () => {
+        const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
+            articlePage: {
+                page: 2,
+                ids: [],
+                entities: {},
+                limit: 5,
+                isLoading: false,
+                hasMore: true,
+            },
+        });
 
-//         await thunk.callThunk();
+        await thunk.callThunk();
 
-//         expect(thunk.dispatch).toBeCalledTimes(4);
-//         expect(ArticlePageActions.setPage).toHaveBeenCalledWith(3);
-//     });
+        expect(thunk.dispatch).toBeCalledTimes(4);
+        expect(ArticlePageActions.setPage).toHaveBeenCalledWith(3);
+    });
 
-//     test('fetchArticleList not be called', async () => {
-//         const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
-//             articlePage: {
-//                 page: 2,
-//                 ids: [],
-//                 entities: {},
-//                 limit: 5,
-//                 isLoading: false,
-//                 hasMore: false,
-//             },
-//         });
+    test('fetchArticleList not be called', async () => {
+        const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
+            articlePage: {
+                page: 2,
+                ids: [],
+                entities: {},
+                limit: 5,
+                isLoading: false,
+                hasMore: false,
+            },
+        });
 
-//         await thunk.callThunk();
+        await thunk.callThunk();
 
-//         expect(thunk.dispatch).toBeCalledTimes(2);
-//         expect(fetchArticleList).not.toHaveBeenCalled();
-//     });
-// });
+        expect(thunk.dispatch).toBeCalledTimes(2);
+        expect(fetchArticleList).not.toHaveBeenCalled();
+    });
+});
