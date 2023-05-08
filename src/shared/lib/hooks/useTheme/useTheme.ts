04d256ee1a07dc686699f0004928708e09a11f12
Andrey Chipizubov <andryukha.chipizybov@mail.ru>
2023-05-09 00:30:55 +0300

    fix

--- src/shared/lib/hooks/useTheme/useTheme.ts
+++ src/shared/lib/hooks/useTheme/useTheme.ts
@@ -4,8 +4,8 @@ import { ThemeContext } from '../../context/ThemeContext';
 import { Theme } from '../../../const/theme';
 
 interface UseThemeResult {
-  toggleTheme: () => void
-  theme: Theme
+    toggleTheme: () => void;
+    theme: Theme;
 }
 
 export function useTheme(): UseThemeResult {
@@ -14,13 +14,17 @@ export function useTheme(): UseThemeResult {
     const toggleTheme = () => {
         let newTheme: Theme;
         switch (theme) {
-        case Theme.DARK: newTheme = Theme.LIGHT;
-            break;
-        case Theme.LIGHT: newTheme = Theme.BLACK;
-            break;
-        case Theme.BLACK: newTheme = Theme.DARK;
-            break;
-        default: newTheme = Theme.BLACK;
+            case Theme.DARK:
+                newTheme = Theme.LIGHT;
+                break;
+            case Theme.LIGHT:
+                newTheme = Theme.BLACK;
+                break;
+            case Theme.BLACK:
+                newTheme = Theme.DARK;
+                break;
+            default:
+                newTheme = Theme.BLACK;
         }
         setTheme?.(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
